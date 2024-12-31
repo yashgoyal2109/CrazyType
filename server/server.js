549c2bce0214e5e4d6c3c01d8eaf521c532c754b
yashgoyal2109 <yashgoyal2109@gmail.com>
2025-01-01 00:52:55 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.ALLOWED_ORIGIN.split(","),
+    origin: process.env.ALLOWED_ORIGIN,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
