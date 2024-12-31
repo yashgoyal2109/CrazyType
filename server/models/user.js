3031f8e566a563e293d5e2b348c41cf21066ad42
yashgoyal2109 <yashgoyal2109@gmail.com>
2024-12-31 23:29:46 +0530

    commit

--- server/models/user.js
+++ server/models/user.js
@@ -18,7 +18,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
